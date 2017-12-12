955f3aaa58950a15dbcd20e2e322081e1660bec4
ThirdOfFive <jrthirdoffive@gmail.com>
2017-12-12 15:54:43 -0600

    Added image textures, changed "game over" mechanic, start screen and high scores in progress

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Pixel_Ball' });
+});
+
+/* GET singleplayer game. */
+router.get('/sp', function(req, res, next) {
+  res.render('singleplayer', { title: 'Pixel_Ball SP'});
 });
 
 module.exports = router;
