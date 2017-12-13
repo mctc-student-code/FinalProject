713f676a9eef07f6a1887b453de5ce75d9f8ab21
ThirdOfFive <jrthirdoffive@gmail.com>
2017-12-12 18:50:38 -0600

    Added image menu icons, finished start menu

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,17 @@ router.get('/', function(req, res, next) {
 
 /* GET singleplayer game. */
 router.get('/sp', function(req, res, next) {
-  res.render('singleplayer', { title: 'Pixel_Ball SP'});
+  res.render('singleplayer', { title: 'Pixel_Ball'});
+});
+
+/* GET multiplayer client page. */
+router.get('/mpclient', function(req, res, next) {
+  res.render('multiplayer', { title: 'Pixel_Ball Multiplayer'});
+});
+
+/* GET leaderboard */
+router.get('/lb', function(req, res, next) {
+  res.render('leaderboard', { title: 'Pixel_Ball Leaderboard'});
 });
 
 module.exports = router;
